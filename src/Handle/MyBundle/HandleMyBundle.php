3e4c18deb4c3afd8a739b8c85f045136a3989c68
fejku <przecinek45@o2.pl>
2015-06-28 16:03:32 +0200

    Added .gitignore file Removed unnecessary files.

--- src/Handle/MyBundle/HandleMyBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Handle\MyBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class HandleMyBundle extends Bundle {
-  
-}
--- src/Moje/Demo123Bundle/MojeDemo123Bundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Moje\Demo123Bundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class MojeDemo123Bundle extends Bundle
-{
-}
